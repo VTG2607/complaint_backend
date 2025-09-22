7e9eb7bfcd060e11dea9963b0c6821e9204252ec
VTG2607 <tr260705@gmail.com>
2025-09-22 22:42:42 +0200

    changed settings for signup

--- complaint_backend/settings.py
+++ complaint_backend/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://vtg2607.github.io/complaint_frontend",
+    "https://vtg2607.github.io",
     "http://localhost:3000",
     "http://localhost:8000",
     "http://localhost:5173",
@@ -85,7 +85,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5000",
-    "https://vtg2607.github.io/complaint_frontend",
+    "https://vtg2607.github.io",
 ]
 ROOT_URLCONF = "complaint_backend.urls"
 
